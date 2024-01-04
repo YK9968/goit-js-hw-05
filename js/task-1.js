d1bc16fca0ecd5d1f484b568151f4b9ba3fc100e
YK9968 <robka4an@gmail.com>
2024-01-04 15:42:21 +0200

    finish HW

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const getUserNames = users => {
-  users.map(user => user.name);
-};
-
+const getUserNames = users => users.map(user => user.name);
 console.log(
   getUserNames([
     {
